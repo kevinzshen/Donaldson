03805f3b2011678fb21bbec4fd69d5d9e8ed2a63
Kevin Shen <kevinzshen3@gmail.com>
2024-09-28 22:50:20 -0400

    Implement R script for downloading dataset

---
+++ scripts/01-download_data.R
@@ -0,0 +1,17 @@
+library(opendatatoronto)
+library(tidyverse)
+set.seed(2620) # last 4 digits of student number used for seed
+
+# Get package
+package <- show_package("b68cb71b-44a7-4394-97e2-5d2f41462a5d")
+
+# Retrieve resources
+resources <- list_package_resources("b68cb71b-44a7-4394-97e2-5d2f41462a5d")
+datastore_resources <- filter(resources, tolower(format) == 'xlsx')
+
+# Load TTC data for delays in the year of 2022
+ttc_delay_data_2022 <- filter(datastore_resources,
+                          name == "ttc-streetcar-delay-data-2022") %>% get_resource()
+
+# Save data
+write_csv(delay_data_2023, "data/raw_data/unedited_ttc_streetcar_delay_2022.csv") 
